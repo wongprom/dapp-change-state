615716b205361f3c3cfe5027356ffb309418827c
wongprom <>
2022-03-31 13:59:56 +0200

    Create new .sol files, a lot of new things...
    create ChangeStateInput.sol and paste the same code we created in remix.ethereum.org. Makae tweaks and new functions in Migrations.sol. Create a new file in migrations folder.

---
+++ contracts/ChangeStateInput.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.26 <0.9.0;
+
+contract ChangStateInput {
+    // Variable
+    uint256 stateInput;
+
+    // Setter function to change variable on chain
+    function set(uint256 v) public {
+        stateInput = v;
+    }
+    
+    // Getter to get variable from chain
+    function get() public view returns (uint256){
+        return stateInput;
+    }
+
+}
