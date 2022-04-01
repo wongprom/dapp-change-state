fee7ee780a97666196bac3287c0552693a3a934e
wongprom <>
2022-04-01 22:15:18 +0200

    Make transaction possible, minor varable name changes and delet out-comment code.

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,12 @@ declare let window: any
 
 //Truffle outputs post-migrations process
 const web3 = new Web3(Web3.givenProvider)
-const contractAddr = '0xfC9B69690ee8A15A2cB6d37Bf2019E317CBc84Cb'
+const contractAddr = '0xDce81b193DA8238b27ecfe3EBec0b1522AF0c7ef'
 // @ts-ignore
 const ChangeState = new web3.eth.Contract(changeStateInput, contractAddr)
 
 const Home: NextPage = () => {
-  const [blockchainData, setBlockchainData] = useState(null)
+  const [blockchainState, setBlockchainState] = useState(null)
   const inputRef = useRef<HTMLInputElement | null>(null)
 
   // check if a web3 instance is running on port:9545
@@ -28,33 +28,29 @@ const Home: NextPage = () => {
     .catch((e) => console.error('Something went wrong : ', e))
 
   // Read: and get data from our local blockchain
-  const handleGetBlockchainData = async (e: any) => {
+  const handleGetBlockchainState = async (e: any) => {
     e.preventDefault()
     const result = await ChangeState.methods.get().call()
-    // setBlockchainData(result)
+    setBlockchainState(result)
     console.log('result: ', result)
   }
   // Write: send data to our local blockchain
   const handleSet = async (e: any) => {
+    if (inputRef === null) return
+
     e.preventDefault()
     const accounts = await window.ethereum.enable()
     const account = accounts[0]
-    const gas = await ChangeState.methods.set(inputRef).estimateGas()
-    const result = await ChangeState.methods.set(inputRef).send({
+    const gas = await ChangeState.methods
+      .set(inputRef.current?.value)
+      .estimateGas()
+    const result = await ChangeState.methods.set(inputRef.current?.value).send({
       from: account,
       gas,
     })
-    console.log(result)
+    console.log('Result from setting blockchain state : ', result)
+    inputRef.current!.value = ''
   }
-
-  // const handleSetBlockchainState = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   e.preventDefault()
-  //   if (inputRef.current?.value) {
-  //     //Set blockchain state
-  //     console.log('inputRef.current?.value ', inputRef.current.value)
-  //     inputRef.current.value = ''
-  //   }
-  // }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -62,7 +58,6 @@ const Home: NextPage = () => {
         <title>DApp change state</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <section className="body-font bg-gray-900 text-gray-400">
           <div className="container mx-auto flex flex-wrap items-center px-5 py-24">
@@ -71,13 +66,15 @@ const Home: NextPage = () => {
                 Get state from block chain
               </h1>
               <p className="mt-4 mb-4 leading-relaxed">
-                Display state from block chain here
+                {blockchainState
+                  ? `Blockchain state: ${blockchainState}`
+                  : "Click 'Get State'"}
               </p>
               <button
-                onClick={(e) => handleGetBlockchainData(e)}
+                onClick={(e) => handleGetBlockchainState(e)}
                 className="rounded border-0 bg-pink-500 py-2 px-8 text-lg text-white hover:bg-pink-600 focus:outline-none"
               >
-                Get state
+                Get State
               </button>
             </div>
             <div className="mt-10 flex w-full flex-col rounded-lg bg-gray-800 bg-opacity-50 p-8 md:ml-auto md:mt-0 md:w-1/2 lg:w-2/6">
@@ -92,7 +89,6 @@ const Home: NextPage = () => {
                   Type a number
                 </label>
                 <input
-                  // value={inputRef}
                   ref={inputRef}
                   type="number"
                   id="number-input"
@@ -100,35 +96,16 @@ const Home: NextPage = () => {
                   className="w-full rounded border border-gray-600 bg-gray-600 bg-opacity-20 py-1 px-3 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-pink-500 focus:bg-transparent focus:ring-2 focus:ring-pink-900"
                 />
               </div>
-              {/* <div className="relative mb-4">
-                <label
-                  htmlFor="email"
-                  className="text-sm leading-7 text-gray-400"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full rounded border border-gray-600 bg-gray-600 bg-opacity-20 py-1 px-3 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-pink-500 focus:bg-transparent focus:ring-2 focus:ring-pink-900"
-                />
-              </div> */}
               <button
-                className="rounded border-0 bg-pink-500 py-2 px-8 text-lg text-white hover:bg-pink-600 focus:outline-none"
-                // onClick={(e) => handleSetBlockchainState(e)}
+                className="rounded border-0 bg-blue-500 py-2 px-8 text-lg text-white hover:bg-pink-600 focus:outline-none"
+                onClick={(e) => handleSet(e)}
               >
                 Set State
               </button>
-              <p className="mt-3 text-xs">Some fancy text</p>
             </div>
           </div>
         </section>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <p>footer</p>
-      </footer>
     </div>
   )
 }
