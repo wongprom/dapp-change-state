04faf1ef85c6b4a5e8d6037659593890580620ff
wongprom <>
2022-04-01 23:59:43 +0200

    Minor clean up befor push

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,17 +27,17 @@ const Home: NextPage = () => {
     .then(() => console.log('connection is successful!'))
     .catch((e) => console.error('Something went wrong : ', e))
 
-  // Read: and get data from our local blockchain
+  // Read: get data from our local blockchain
   const handleGetBlockchainState = async (e: any) => {
     e.preventDefault()
     const result = await ChangeState.methods.get().call()
     setBlockchainState(result)
     console.log('result: ', result)
   }
+
   // Write: send data to our local blockchain
   const handleSet = async (e: any) => {
     if (inputRef === null) return
-
     e.preventDefault()
     const accounts = await window.ethereum.enable()
     const account = accounts[0]
@@ -53,17 +53,17 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-300 py-2">
       <Head>
         <title>DApp change state</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <section className="body-font bg-gray-900 text-gray-400">
+      <main className="flex w-full flex-1 flex-col items-center justify-center  px-20 text-center">
+        <section className="body-font rounded-lg bg-gray-900 text-gray-400">
           <div className="container mx-auto flex flex-wrap items-center px-5 py-24">
-            <div className="pr-0 md:w-1/2 md:pr-16 lg:w-3/5 lg:pr-0">
+            <div className="mx-auto pr-0 md:w-1/2 md:pr-16 lg:w-3/5 lg:pr-0">
               <h1 className="title-font text-3xl font-medium text-white">
-                Get state from block chain
+                Get state from blockchain
               </h1>
               <p className="mt-4 mb-4 leading-relaxed">
                 {blockchainState
@@ -79,7 +79,7 @@ const Home: NextPage = () => {
             </div>
             <div className="mt-10 flex w-full flex-col rounded-lg bg-gray-800 bg-opacity-50 p-8 md:ml-auto md:mt-0 md:w-1/2 lg:w-2/6">
               <h2 className="title-font mb-5 text-lg font-medium text-white">
-                Set state on block chain
+                Set state on blockchain
               </h2>
               <div className="relative mb-4">
                 <label
@@ -97,7 +97,7 @@ const Home: NextPage = () => {
                 />
               </div>
               <button
-                className="rounded border-0 bg-blue-500 py-2 px-8 text-lg text-white hover:bg-pink-600 focus:outline-none"
+                className="rounded border-0 bg-pink-500 py-2 px-8 text-lg text-white hover:bg-pink-600 focus:outline-none"
                 onClick={(e) => handleSet(e)}
               >
                 Set State
